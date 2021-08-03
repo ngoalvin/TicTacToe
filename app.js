3628e80eedca848f72bfd40d23d19feaee0901d0
Alvin Ngo <ngoalvin97@gmail.com>
2021-08-03 00:25:29 -0400

    finished all functionality

--- app.js
+++ app.js
@@ -1,15 +1,28 @@
 const status = document.querySelector('.status');
 const gridSquares = document.querySelectorAll('.grid-square');
 const resetButton = document.querySelector('.reset');  
+const modalBg = document.querySelector('.modal-bg');
+const modalBg2 = document.querySelector('.modal-bg2');
+const winnerDiv = document.querySelector('.winner');
+const error = document.querySelector('.error');
 
 let xIsNext = true;
 let winner = null;
+let mode = null;
+let randomOrder = [];
+
+const generateRandomOrder = () => {
+    while (randomOrder.length !== 9) {
+        let r = Math.floor(Math.random() * 9);
+        if(randomOrder.indexOf(r) === -1) randomOrder.push(r);
+        console.log(randomOrder)
+    }
+}
 
 const checkHoriz = () => {
     for (let i = 0; i < 7; i+=3) {
         if (gridSquares[i].classList[1] && gridSquares[i].classList[1] === gridSquares[i + 1].classList[1] && gridSquares[i].classList[1] === gridSquares[i + 2].classList[1]) {
             winner = gridSquares[i].classList[1];
-            console.log(winner)
         }
     }
 }
@@ -18,7 +31,6 @@ const checkVert = () => {
     for (let i = 0; i < 3; i++) {
         if (gridSquares[i].classList[1] && gridSquares[i].classList[1] === gridSquares[i + 3].classList[1] && gridSquares[i].classList[1] === gridSquares[i + 6].classList[1]) {
             winner = gridSquares[i].classList[1];
-            console.log(winner)
         }
     }
 }
@@ -49,26 +61,97 @@ const checkWinner = () => {
     checkTie();
 }
 
+const gameStatus = () => {
+    checkWinner();
+    if (winner) {
+        if (winner === 'tie') {
+            let text = document.createTextNode(`The game ended in a tie!`)
+            modalBg.classList.add('bg-active');
+
+            winnerDiv.appendChild(text)
+        } else {
+            let text = document.createTextNode(`${winner} has won the game!`)
+            modalBg.classList.add('bg-active');
+    
+            winnerDiv.appendChild(text)
+        }
+    }
+}
+
+const randomMode = () => {
+    mode = 'random';
+    generateRandomOrder();
+    console.log(randomOrder)
+    closeStartModal();
+}
+
+const normalMode = () => {
+    mode = 'normal';
+    closeStartModal();
+}
+
+const closeStartModal = () => {
+    modalBg2.classList.remove('bg-active');
+}
+
 const handleReset = (e) => {
     console.log(e);
-}
+    xIsNext = true;
+    inPlay = true;
+    winner = null;
+    mode = null;
+    randomOrder = [];
+    for (let gridSquare of gridSquares) {
+        gridSquare.classList.remove('blue');
+        gridSquare.classList.remove('yellow');
+    }
+    modalBg.classList.remove('bg-active');
+    winnerDiv.innerText = '';
+    status.innerHTML = 'Blue is next';
+    modalBg2.classList.add('bg-active');
+    error.classList.remove('active');
+};
 
 const handleSquareClick = (e) => {
     const classList = e.target.classList;
-    if (classList[1] || winner) {
-        return;
-    }
-
-    if (xIsNext) {
-        e.target.classList.add('blue');
-        xIsNext = !xIsNext;
-        status.innerHTML = 'Blue is next';
-    } else {
-        e.target.classList.add('yellow');
-        xIsNext = !xIsNext;
-        status.innerHTML = 'Yellow is next';
+    if (mode === 'normal') {
+        if (classList[1] || winner) {
+            error.classList.add('active');
+            return;
+        }
+    
+        if (xIsNext) {
+            e.target.classList.add('blue');
+            error.classList.remove('active');
+            gameStatus();
+            xIsNext = !xIsNext;
+            status.innerHTML = 'Blue is next';
+        } else {
+            e.target.classList.add('yellow');
+            error.classList.remove('active');
+            gameStatus();
+            xIsNext = !xIsNext;
+            status.innerHTML = 'Yellow is next';
+        }
+    } else if (mode === 'random') {
+        if (xIsNext) {
+            gridSquares[randomOrder[randomOrder.length - 1]].classList.add('blue');
+            randomOrder.pop();
+            console.log(randomOrder)
+            gameStatus();
+            xIsNext = !xIsNext;
+            status.innerHTML = 'Blue is next';
+        } else {
+            gridSquares[randomOrder[randomOrder.length - 1]].classList.add('yellow');
+            gameStatus();
+            randomOrder.pop();
+            xIsNext = !xIsNext;
+            status.innerHTML = 'Yellow is next';
+        }
     }
 };
+
+
 resetButton.addEventListener('click', handleReset);
 
 for (gridSquare of gridSquares) {
