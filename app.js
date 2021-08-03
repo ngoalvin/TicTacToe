7ffea1a0aaf75ede659ebaab20652091e1fdd2c3
Alvin Ngo <ngoalvin97@gmail.com>
2021-08-02 23:17:17 -0400

    add X and O clicks

--- app.js
+++ app.js
@@ -3,15 +3,28 @@ const gridSquares = document.querySelectorAll('.grid-square');
 const resetButton = document.querySelector('.reset');  
 
 let xIsNext = true;
+let winner = null;
 
 const handleReset = (e) => {
     console.log(e);
 }
 
 const handleSquareClick = (e) => {
-    console.log(e);
-}
+    const classList = e.target.classList;
+    if (classList[1] || winner) {
+        return;
+    }
 
+    if (xIsNext) {
+        e.target.classList.add('blue');
+        xIsNext = !xIsNext;
+        status.innerHTML = 'Blue is next';
+    } else {
+        e.target.classList.add('yellow');
+        xIsNext = !xIsNext;
+        status.innerHTML = 'Yellow is next';
+    }
+};
 resetButton.addEventListener('click', handleReset);
 
 for (gridSquare of gridSquares) {
